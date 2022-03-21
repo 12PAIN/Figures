cb4536edf6e33507a0c1e2c473ecef8e60994c71
Anatoly <hexagon643@gmail.com>
2022-03-21 08:22:10 +0700

    Test cast to Figures

--- Figures.cpp
+++ Figures.cpp
@@ -4,10 +4,14 @@
 int main(){
     //std::cout << 1;
 
-    Figure* new_figure = new Triangle(1,1,1);
+    Figure* new_figure = new Triangle(2,2,1);
     Prism* new_prism = new TrianglePrism(5,new_figure);
-    delete new_figure;
+    //delete new_figure;
     
+    Triangle* new_tri = static_cast<Triangle*>(new_figure);
+
+    std::cout << new_tri->GetA() << std::endl;
+
     std::cout << new_prism->BaseSpace();    
     
     return 0;
